91cd36c75d2d0d13d68ac1c21f19940d12807006
t.heinze <theinze@users.noreply.github.com>
2020-07-30 16:07:33 +0200

    report generation

--- report.sh
+++ report.sh
@@ -4,10 +4,11 @@
 cd ~/sastevaluation/juliet_c_13
 sudo python3 import_log.py
 sudo python3 report.py
+mv report0.html ../report.html
 
 echo -e "###################################################################################### "
 echo -e "###################################################################################### "
-echo -e "Evaluation Report Generation Complete"
+echo -e "Generated SAST for C Evaluation Report in: report.html"
 echo -e "###################################################################################### "
 echo -e "###################################################################################### "
 
