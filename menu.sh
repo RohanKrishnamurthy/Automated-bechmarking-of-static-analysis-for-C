605bec1d01cfe7f1123e681042020320c7819b59
RohanKrishnamurthy <rohan.k.14@gmail.com>
2020-09-16 13:09:54 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,6 +9,17 @@
 
 echo -e "###################################################################################### "
 echo -e "###################################################################################### "
+echo -e "Setting the environment for evaluation"
+echo -e "###################################################################################### "
+
+export CLANG_LIBRARY_PATH="/usr/lib/llvm-4.0/lib"
+export C_INCLUDE_PATH="/usr/lib/gcc/x86_64-linux-gnu/7/include:/usr/lib/gcc/x86_64-linux-gnu/7/include-fixed:/usr/include/x86_64-linux-gnu:/usr/include"
+
+cp -r ~/sastevaluation/C/testcasesupport ~/sastevaluation/juliet_c_13/runners/
+cp -r ~/sastevaluation/C/testcases ~/sastevaluation/juliet_c_13/runners/
+
+echo -e "###################################################################################### "
+echo -e "###################################################################################### "
 echo -e "Choice of tools for Evaluation"
 echo -e "###################################################################################### "
 echo -e "###################################################################################### "
@@ -33,7 +44,7 @@ case $opt in
           break
           ;;
 	 "clang-tidy")
-	   sudo apt install -y clang-tidy-4.0
+	   sudo apt install -y clang-4.0 clang-tidy-4.0
 	   cd ~/sastevaluation/juliet_c_13/runners/
 	   chmod +x run_clang_tidy.sh
 	  ./run_clang_tidy.sh
@@ -47,7 +58,6 @@ case $opt in
           ;;
 	    "frama-c")
           sudo apt install -y frama-c
-          cp -r ~/sastevaluation/C/testcasesupport ~/sastevaluation/juliet_c_13/runners/
           cd ~/sastevaluation/juliet_c_13/runners/
     	  ./run_framac.sh
           break
@@ -61,7 +71,6 @@ case $opt in
           ;;
 	    "sparse")
 	      sudo apt install -y sparse
-          cp -r ~/sastevaluation/C/testcasesupport ~/sastevaluation/juliet_c_13/runners/
           cd ~/sastevaluation/juliet_c_13/runners/
           ./run_sparse.sh
           break
@@ -70,8 +79,6 @@ case $opt in
 	  cd ~/sastevaluation/juliet_c_13/runners/
 	  chmod +x install_ikos.sh
           ./install_ikos.sh
-          cp -r ~/sastevaluation/C/testcasesupport ~/sastevaluation/juliet_c_13/runners/
-          cp -r ~/sastevaluation/C/testcases ~/sastevaluation/juliet_c_13/runners/
           cd ~/sastevaluation/juliet_c_13/runners/
           ./run_ikos.sh
           break
@@ -80,8 +87,6 @@ case $opt in
           cd sastevaluation
           wget https://github.com/facebook/infer/releases/download/v0.17.0/infer-linux64-v0.17.0.tar.xz
           tar -xvJf infer-linux64-v0.17.0.tar.xz && rm infer-linux64-v0.17.0.tar.xz
-          cp -r ~/sastevaluation/C/testcasesupport ~/sastevaluation/juliet_c_13/runners/
-          cp -r ~/sastevaluation/C/testcases ~/sastevaluation/juliet_c_13/runners/
           cd ~/sastevaluation/juliet_c_13/runners/
 	  ./run_infer.sh
           break
@@ -90,8 +95,6 @@ case $opt in
           cd sastevaluation
           wget https://github.com/oclint/oclint/releases/download/v0.13.1/oclint-0.13.1-x86_64-linux-4.4.0-112-generic.tar.gz 
           tar -zxvf oclint-0.13.1-x86_64-linux-4.4.0-112-generic.tar.gz && rm oclint-0.13.1-x86_64-linux-4.4.0-112-generic.tar.gz
-          cp -r ~/sastevaluation/C/testcasesupport ~/sastevaluation/juliet_c_13/runners/
-          cd ~/sastevaluation/juliet_c_13/runners/
           ./run_oclint.sh
           break
           ;;
@@ -99,8 +102,6 @@ case $opt in
           sudo apt-get install -y gcc-6
           sudo apt install -y ruby-full
           sudo gem install -y adlint --no-ri --no-rdoc
-          cp -r ~/sastevaluation/C/testcasesupport ~/sastevaluation/juliet_c_13/runners/
-          cd ~/sastevaluation/juliet_c_13/runners/
           ./run_adlint.sh
           break
           ;;
